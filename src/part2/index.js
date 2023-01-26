7f981f005026adddef92677f16b03b2487df5f82
Ioanna-Istomina <99349464+Ioanna-Istomina@users.noreply.github.com>
2023-01-26 18:21:05 +0200

    created folder "part2"

---
+++ src/part2/index.js
@@ -0,0 +1,16 @@
+const someMysticMethod = (param): string => {
+    if (!param) {
+        console.log("Error: some error text...");
+        return ''
+    }
+
+    const words = param.split(' ');
+
+    if(words.length > 1){
+        const firstWord = words[0];
+        const lastWord = words[words.length - 1];
+        return (firstWord[0] + lastWord[0]).toUpperCase();
+    }else{
+       return param[0]? param[0].toUpperCase() : '';
+    }
+}
